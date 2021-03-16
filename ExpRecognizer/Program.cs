cea31ac4b1e07487c17d9340d6564f6aaedbb899
Станислав Бобиков <xanitus2000@mail.ru>
2021-03-17 00:59:20 +0300

    can parse exp without func

--- ExpRecognizer/Program.cs
+++ ExpRecognizer/Program.cs
@@ -13,9 +13,16 @@ namespace ExpRecognizer
 
             //2+2
             //2+2+2
-            string exp = "(2*sin(x)+4)/fun(2)*(4+2)+245*x";
+            //string exp = "(2*sin(x)+4)/fun(2)*(4+2)+245*x";
 
-            Console.WriteLine((new ContextParser()).Parse("3+4*2/(1-5)^2^3"));
+            var result = (new Tokenizer()).Split("2+2 -4 * 7 + (6+7 *8 / (3+4))^2");
+
+            //foreach (var item in result) //"2+2.2^2- asdf+sin((2 +3),a, b, 3)-456 * 7 + (6+7821 *8 / (3+4))^2"))
+            //{
+            //    Console.WriteLine(item.Value + " " + item.Type.ToString());
+            //}
+
+            Console.WriteLine((new ContextParser()).Parse(result));
 
             //выделить блоки, разбить, парситьы
             //парсить выражения в скобках
@@ -24,16 +31,18 @@ namespace ExpRecognizer
 
     public struct Operation : IComparable<Operation>
     {
-        public readonly char Type;
+        public readonly string Type;
         public readonly int Priority;
+        public readonly bool LeftSided;
 
-        public Operation(char operation, int priority)
+        public Operation(string operation, int priority, bool leftsided = true)
         {
             Type = operation;
             Priority = priority;
+            LeftSided = leftsided;
         }
 
-        public bool Is(char type) 
+        public bool Is(string type)
         {
             return type == Type;
         }
@@ -44,242 +53,227 @@ namespace ExpRecognizer
         }
     }
 
-    public class ContextParser 
+
+
+    public enum TokenType { Number, Variable, Function, OpenBracket, CloseBracket, Operator, Comma, Point, WhiteSpace, None };
+
+    public struct Token
     {
-        private Stack<char> operationsStack;
-        private Dictionary<char, Operation> operations;
+        public readonly TokenType Type;
+        public readonly string Value;
+
+        public Token(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+
+    public class Tokenizer
+    {
+        private StringBuilder buffer;
+
+        public Tokenizer()
+        {
+            buffer = new StringBuilder();
+        }
+
+        public IEnumerable<Token> Split(string expression)
+        {
+            buffer.Clear();
+            TokenType next = TokenType.None, prev = TokenType.None;
+
+            foreach (var item in expression)
+            {
+                prev = next;
+                next = DetermineType(item);
+                if (next == TokenType.OpenBracket && prev == TokenType.OpenBracket ||
+                       next == TokenType.CloseBracket && prev == TokenType.CloseBracket)
+                {
+                    yield return new Token(prev, buffer.ToString());
+                    buffer.Clear();
+                }
+                else if (next != prev)
+                {
+                    if (next == TokenType.OpenBracket && prev == TokenType.Variable)
+                    {
+                        yield return new Token(TokenType.Function, buffer.ToString());
+                        buffer.Clear();
+                    }
+                    else if (next == TokenType.Point && prev == TokenType.Number)
+                    {
+                        next = TokenType.Number;
+                    }
+                    else
+                    {
+                        yield return new Token(prev, buffer.ToString());
+                        buffer.Clear();
+                    }
+                }
+                buffer.Append(item);
+            }
+            if (buffer.Length != 0)
+            {
+                yield return new Token(next, buffer.ToString());
+            }
+        }
+
+        private TokenType DetermineType(char ch)
+        {
+            if (char.IsLetter(ch))
+                return TokenType.Variable;
+            if (char.IsDigit(ch))
+                return TokenType.Number;
+            if (char.IsWhiteSpace(ch))
+                return TokenType.WhiteSpace;
+            if (ch == ',')
+                return TokenType.Comma;
+            if (ch == '.')
+                return TokenType.Point;
+            if (ch == '(')
+                return TokenType.OpenBracket;
+            if (ch == ')')
+                return TokenType.CloseBracket;
+            if (ContextParser.ContainsOperation(ch.ToString()))
+                return TokenType.Operator;
+
+            throw new Exception("Wrong character");
+        }
+    }
+
+
+
+
+    public class ContextParser
+    {
+        private Stack<string> operationsStack;
+        private static Dictionary<string, Operation> operations;
         private StringBuilder output;
         private StringBuilder buffer;
 
-        public ContextParser()
+        static ContextParser()
         {
-            operationsStack = new Stack<char>();
-            output = new StringBuilder();
-            buffer = new StringBuilder();
-            operations = new Dictionary<char, Operation>()
+            operations = new Dictionary<string, Operation>()
             {
-                {'+', new Operation('+', 2) },
-                {'-', new Operation('-', 2) },
-                {'/', new Operation('/', 4) },
-                {'*', new Operation('*', 4) },
-                {'^', new Operation('^', 6) }
+                {"+", new Operation("+", 2) },
+                {"-", new Operation("-", 2) },
+                {"/", new Operation("/", 4) },
+                {"*", new Operation("*", 4) },
+                {"^", new Operation("^", 6, false) }
             };
         }
 
+        public ContextParser()
+        {
+            operationsStack = new Stack<string>();
+            output = new StringBuilder();
+            buffer = new StringBuilder();
+        }
 
-        public string Parse(string exp) 
+        public static bool ContainsOperation(string operation)
+        {
+            return operations.ContainsKey(operation);
+        }
+
+        public string Parse(IEnumerable<Token> tokens)
         {
             Reset();
-            Process(exp);
-            ClearBuffer();
+            Process(tokens);
             Finish();
             return output.ToString();
         }
 
-        private void Reset() 
+        private void Reset()
         {
-            buffer.Clear();
+            operationsStack.Clear();
             output.Clear();
         }
 
-        private void Process(string expression) 
+        private void Process(IEnumerable<Token> tokens)
         {
-            foreach (var symbol in expression)
+            foreach (var token in tokens)
             {
-                if (IsPartOfNumber(symbol))
+                switch (token.Type)
                 {
-                    buffer.Append(symbol);
-                }
-                else if (IsBinaryOperator(symbol))
-                {
-                    CompletePart();
-                    HandleOperation(symbol);
-                }
-                else if (symbol == '(')
-                {
-                    operationsStack.Push(symbol);
-                }
-                else if (symbol == ')') 
-                {
-                    HandleCloseBracket();
+                    case (TokenType.Number):
+                    case (TokenType.Variable):
+                        {
+                            ToOutput(token.Value);
+                            break;
+                        }
+                    case (TokenType.Function):
+                    case (TokenType.OpenBracket):
+                        {
+                            operationsStack.Push(token.Value);
+                            break;
+                        }
+                    case (TokenType.CloseBracket):
+                        {
+                            HandleCloseBracket();
+                            break;
+                        }
+                    case (TokenType.Operator):
+                        {
+                            HandleOperation(token.Value);
+                            break;
+                        }
+                    case (TokenType.Comma):
+                        { // TODO
+                            break;
+                        }
                 }
             }
         }
 
-        private void Finish() 
+        private void Finish()
         {
-            while (IsOperationExist(operationsStack)) 
+            while (IsOperationExist(operationsStack))
+            {
+                ToOutput(operationsStack.Pop());
+            }
+        }
+
+        private void HandleCloseBracket()
+        {
+            while (operationsStack.Peek() != "(") 
+            {
+                ToOutput(operationsStack.Pop());
+            }
+            operationsStack.Pop();
+        }
+
+        private void HandleOperation(string operation)
+        {
+            if (!IsOperationExist(operationsStack))
+            {
+                operationsStack.Push(operation);
+                return;
+            }
+            if(operationsStack.Peek() == "(") 
+            {
+
+                operationsStack.Push(operation);
+                return;
+            }
+            while (IsOperationExist(operationsStack) && (operations[operation].CompareTo(operations[operationsStack.Peek()]) < 0 ||
+                    operations[operation].CompareTo(operations[operationsStack.Peek()]) == 0 && operations[operation].LeftSided))
             {
                 output.Append(operationsStack.Pop());
                 output.Append(' ');
             }
+
+            operationsStack.Push(operation);
         }
 
-        private bool IsPartOfNumber(char symbol)
+        private bool IsOperationExist(Stack<string> operationsStack)
         {
-            return symbol >= '0' && symbol <= '9' || symbol == ',';
+            return operationsStack.Count != 0;
         }
 
-        private bool IsBinaryOperator(char symbol) 
+        private void ToOutput(string value)
         {
-            return operations.Keys.Contains(symbol);
-        }
-
-        private void ClearBuffer() 
-        {
-            if (buffer.Length != 0)
-            {
-                output.Append(buffer.ToString());
-                output.Append(' ');
-            }
-        }
-
-        private void CompletePart()
-        {
-            if (buffer.Length != 0)
-            {
-                output.Append(buffer.ToString());
-                output.Append(' ');
-                buffer.Clear();
-            }
-        }
-
-        private void HandleOperation(char operation) 
-        {
-            if (IsOperationExist(operationsStack) && operationsStack.Peek() != '(')
-            {
-                if ( operations[operation].CompareTo(operations[operationsStack.Peek()]) <= 0) // operation rather
-                {
-                    while (operations[operation].CompareTo(operations[operationsStack.Peek()]) <= 0) 
-                    {
-                        operationsStack.Pop();
-                    }
-                    operationsStack.Push(operation);
-                }
-                else // (operation.CompareTo(operationsStack.Peek()) == 0) 
-                {
-                    output.Append(operationsStack.Pop());
-                    output.Append(' ');
-                    operationsStack.Push(operation);
-                }
-            }
-            else operationsStack.Push(operation);
-        }
-
-        private bool IsOperationExist(Stack<char> operations) 
-        {
-            return operations.Count != 0;
-        }
-
-        private void HandleCloseBracket() 
-        {
-            var current = operationsStack.Pop();
-            while (current != '(') 
-            {
-                current = operationsStack.Pop();
-            }
-            operationsStack.Pop();
+            output.Append(value);
+            output.Append(' ');
         }
     }
-
-
-    public abstract class Expression 
-    {
-        public abstract float Calculate();
-    }
-
-
-
-
-
-
-
-    //public class Context 
-    //{
-    //    private StringBuilder builder;
-    //    private string fullExpression;
-
-    //    public string GetCurrentPart() 
-    //    { }
-    //}
-
-    //public class ContextSpliter 
-    //{
-    //    private Expression<float> head;
-
-
-    //}
-
-    //public interface Expression<T>
-    //{
-    //    void Interpret(Context context);
-    //    T Calculate();
-    //}
-
-
-    //public abstract class NonTerminalExpression<T> : Expression<T>
-    //{
-    //    protected Expression<T> left;
-    //    protected Expression<T> right;
-
-    //    public T Calculate() 
-    //    {
-    //        try
-    //        {
-    //            return Calculate(left.Calculate(), right.Calculate());
-    //        }
-    //        catch (Exception exc) 
-    //        {
-    //            //TODO: exc
-    //            throw new Exception();
-    //        }
-    //    }
-
-
-    //    public abstract void Interpret(Context context);
-
-    //    protected abstract T Calculate(T valueLeft, T valueRight);
-    //}
-
-    //public class SumExpression : NonTerminalExpression<float>
-    //{
-    //    public override void Interpret(Context context)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    protected override float Calculate(float valueLeft, float valueRight)
-    //    {
-    //        return valueLeft + valueRight;
-    //    }
-    //}
-
-
-    //public class IntTerminalExpression : Expression<int>
-    //{
-    //    public int Calculate()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void Interpret(Context context)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //}
-
-    //public class FloatTerminalExpression: Expression<float>
-    //{
-    //    private float value;
-    //    public void Interpret(Context context)
-    //    {
-            
-    //    }
-
-    //    public float Calculate()
-    //    {
-    //        return 0;
-    //    }
-    //}
-
 }
