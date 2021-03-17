e7cd5e6a9bb00536a6f2b64d207554dd9789d0e5
Станислав Бобиков <xanitus2000@mail.ru>
2021-03-18 02:29:31 +0300

    it's workinggit add .!

--- ExpRecognizer/Program.cs
+++ ExpRecognizer/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using static ConsoleApp6.ExpressionTreeCreator;
 
-namespace ExpRecognizer
+namespace ConsoleApp6
 {
     class Program
     {
@@ -15,15 +16,26 @@ namespace ExpRecognizer
             //2+2+2
             //string exp = "(2*sin(x)+4)/fun(2)*(4+2)+245*x";
 
+            //"sin(max(2, 3 ) / 3 * Pi)"
+            //"2+2 -4 * 7 + (6+7 *8 / (3+4))^2"
+            //"2 + 5* PI * (sin(3+4* (1 -3))) /2^max(3,4)"
+
             var result = (new Tokenizer()).Split("2+2 -4 * 7 + (6+7 *8 / (3+4))^2");
 
             //foreach (var item in result) //"2+2.2^2- asdf+sin((2 +3),a, b, 3)-456 * 7 + (6+7821 *8 / (3+4))^2"))
             //{
             //    Console.WriteLine(item.Value + " " + item.Type.ToString());
             //}
+            //Console.WriteLine();
 
-            Console.WriteLine((new ContextParser()).Parse(result));
+            var tokens = (new ContextParser()).Parse(result);
+            foreach (var item in tokens)
+            {
+                Console.WriteLine(item.Value + " " + item.Type.ToString());
+            }
 
+            var exp = (new ExpressionTreeCreator()).CreateTree(tokens);
+            Console.WriteLine(exp.Calculate());
             //выделить блоки, разбить, парситьы
             //парсить выражения в скобках
         }
@@ -34,12 +46,14 @@ namespace ExpRecognizer
         public readonly string Type;
         public readonly int Priority;
         public readonly bool LeftSided;
+        public readonly int ArgumentCount;
 
-        public Operation(string operation, int priority, bool leftsided = true)
+        public Operation(string operation, int priority, int argc, bool leftsided = true)
         {
             Type = operation;
             Priority = priority;
             LeftSided = leftsided;
+            ArgumentCount = argc;
         }
 
         public bool Is(string type)
@@ -52,11 +66,7 @@ namespace ExpRecognizer
             return this.Priority - other.Priority;
         }
     }
-
-
-
     public enum TokenType { Number, Variable, Function, OpenBracket, CloseBracket, Operator, Comma, Point, WhiteSpace, None };
-
     public struct Token
     {
         public readonly TokenType Type;
@@ -68,7 +78,6 @@ namespace ExpRecognizer
             Value = value;
         }
     }
-
     public class Tokenizer
     {
         private StringBuilder buffer;
@@ -87,8 +96,9 @@ namespace ExpRecognizer
             {
                 prev = next;
                 next = DetermineType(item);
+
                 if (next == TokenType.OpenBracket && prev == TokenType.OpenBracket ||
-                       next == TokenType.CloseBracket && prev == TokenType.CloseBracket)
+                    next == TokenType.CloseBracket && prev == TokenType.CloseBracket)
                 {
                     yield return new Token(prev, buffer.ToString());
                     buffer.Clear();
@@ -118,69 +128,54 @@ namespace ExpRecognizer
             }
         }
 
-        private TokenType DetermineType(char ch)
+        private TokenType DetermineType(char element)
         {
-            if (char.IsLetter(ch))
+            if (char.IsLetter(element))
                 return TokenType.Variable;
-            if (char.IsDigit(ch))
+            if (char.IsDigit(element))
                 return TokenType.Number;
-            if (char.IsWhiteSpace(ch))
+            if (char.IsWhiteSpace(element))
                 return TokenType.WhiteSpace;
-            if (ch == ',')
+            if (element == ',')
                 return TokenType.Comma;
-            if (ch == '.')
+            if (element == '.')
                 return TokenType.Point;
-            if (ch == '(')
+            if (element == '(')
                 return TokenType.OpenBracket;
-            if (ch == ')')
+            if (element == ')')
                 return TokenType.CloseBracket;
-            if (ContextParser.ContainsOperation(ch.ToString()))
+            if (Context.ContainsOperation(element.ToString()))
                 return TokenType.Operator;
 
             throw new Exception("Wrong character");
         }
     }
-
-
-
-
     public class ContextParser
     {
-        private Stack<string> operationsStack;
-        private static Dictionary<string, Operation> operations;
+        private Stack<Token> operationsStack;
         private StringBuilder output;
         private StringBuilder buffer;
 
-        static ContextParser()
-        {
-            operations = new Dictionary<string, Operation>()
-            {
-                {"+", new Operation("+", 2) },
-                {"-", new Operation("-", 2) },
-                {"/", new Operation("/", 4) },
-                {"*", new Operation("*", 4) },
-                {"^", new Operation("^", 6, false) }
-            };
-        }
 
         public ContextParser()
         {
-            operationsStack = new Stack<string>();
+            operationsStack = new Stack<Token>();
             output = new StringBuilder();
             buffer = new StringBuilder();
         }
 
-        public static bool ContainsOperation(string operation)
-        {
-            return operations.ContainsKey(operation);
-        }
-
-        public string Parse(IEnumerable<Token> tokens)
+        public IEnumerable<Token> Parse(IEnumerable<Token> tokens)
         {
             Reset();
-            Process(tokens);
-            Finish();
-            return output.ToString();
+            foreach (var item in Process(tokens))
+            {
+                yield return item;
+            }
+            foreach (var item in Finish())
+            {
+                yield return item;
+            }
+            //return output.ToString();
         }
 
         private void Reset()
@@ -189,7 +184,7 @@ namespace ExpRecognizer
             output.Clear();
         }
 
-        private void Process(IEnumerable<Token> tokens)
+        private IEnumerable<Token> Process(IEnumerable<Token> tokens)
         {
             foreach (var token in tokens)
             {
@@ -198,74 +193,66 @@ namespace ExpRecognizer
                     case (TokenType.Number):
                     case (TokenType.Variable):
                         {
-                            ToOutput(token.Value);
+                            yield return token;
                             break;
                         }
                     case (TokenType.Function):
                     case (TokenType.OpenBracket):
                         {
-                            operationsStack.Push(token.Value);
+                            operationsStack.Push(token);
                             break;
                         }
                     case (TokenType.CloseBracket):
                         {
-                            HandleCloseBracket();
+                            foreach (var item in HandleCloseBracket())
+                            {
+                                yield return item;
+                            }
                             break;
                         }
                     case (TokenType.Operator):
                         {
-                            HandleOperation(token.Value);
-                            break;
-                        }
-                    case (TokenType.Comma):
-                        { // TODO
+                            foreach (var item in HandleOperation(token))
+                            {
+                                yield return item;
+                            }
                             break;
                         }
                 }
             }
         }
 
-        private void Finish()
+        private IEnumerable<Token> Finish()
         {
             while (IsOperationExist(operationsStack))
             {
-                ToOutput(operationsStack.Pop());
+                yield return operationsStack.Pop();
             }
         }
 
-        private void HandleCloseBracket()
+        private IEnumerable<Token> HandleCloseBracket()
         {
-            while (operationsStack.Peek() != "(") 
+            while (IsOperationExist(operationsStack) && operationsStack.Peek().Type != TokenType.OpenBracket)
             {
-                ToOutput(operationsStack.Pop());
+                yield return operationsStack.Pop();
             }
             operationsStack.Pop();
         }
 
-        private void HandleOperation(string operation)
+        private IEnumerable<Token> HandleOperation(Token operation)
         {
-            if (!IsOperationExist(operationsStack))
+            while (IsOperationExist(operationsStack) &&
+                    (operationsStack.Peek().Type != TokenType.OpenBracket) &&
+                    (Context.CompareOperations(operation.Value, operationsStack.Peek().Value) < 0 ||
+                    (Context.CompareOperations(operation.Value, operationsStack.Peek().Value)) == 0 && Context.IsOperationLeftSided(operationsStack.Peek().Value)))
             {
-                operationsStack.Push(operation);
-                return;
-            }
-            if(operationsStack.Peek() == "(") 
-            {
-
-                operationsStack.Push(operation);
-                return;
-            }
-            while (IsOperationExist(operationsStack) && (operations[operation].CompareTo(operations[operationsStack.Peek()]) < 0 ||
-                    operations[operation].CompareTo(operations[operationsStack.Peek()]) == 0 && operations[operation].LeftSided))
-            {
-                output.Append(operationsStack.Pop());
-                output.Append(' ');
+                yield return operationsStack.Pop();
             }
 
             operationsStack.Push(operation);
         }
 
-        private bool IsOperationExist(Stack<string> operationsStack)
+        private bool IsOperationExist(Stack<Token> operationsStack)
         {
             return operationsStack.Count != 0;
         }
@@ -275,5 +262,339 @@ namespace ExpRecognizer
             output.Append(value);
             output.Append(' ');
         }
+    }
+
+
+
+    public static class Context
+    {
+        private static Dictionary<string, Operation> operations;
+        private static Dictionary<string, float> variables;
+        private static Dictionary<string, FunctionExpression> functions;
+
+        static Context()
+        { //TODO - move argc to function
+
+            functions = new Dictionary<string, FunctionExpression>()
+            {
+                {"+", new SumOperator() },
+                {"-", new MinusOperator() },
+                {"^", new PowOperator() },
+                {"*", new MultiplyOperator() },
+                {"/", new DevideOperator() },
+                {"sin", new SinExpression() },
+                {"max", new MaxExpression() },
+            };
+
+            operations = new Dictionary<string, Operation>()
+            {
+                {"+", new Operation("+", 2, 2) },
+                {"-", new Operation("-", 2, 2) },
+                {"/", new Operation("/", 4, 2) },
+                {"*", new Operation("*", 4, 2) },
+                {"^", new Operation("^", 6, 2, false) },
+                {"sin", new Operation("sin", 10, 1, false) },
+                {"max", new Operation("max", 10, 2, false) }
+            };
+
+            variables = new Dictionary<string, float>()
+            {
+                {"Pi", (float)Math.PI }
+            };
+        }
+
+        public static FunctionExpression GetFunction(string name) 
+        {
+            return functions[name].Clone();   
+        }
+
+        public static int CompareOperations(string left, string right)
+        {
+            return operations[left].CompareTo(operations[right]);
+        }
+
+        public static bool ContainsOperation(string operation)
+        {
+            return operations.ContainsKey(operation);
+        }
+
+        public static bool IsOperationLeftSided(string operation)
+        {
+            return operations[operation].LeftSided;
+        }
+
+        public static int GetAgrumentsCount(string operation)
+        {
+            return operations[operation].ArgumentCount;
+        }
+
+        public static float GetVariableValue(string name)
+        {
+            return variables[name];
+        }
+
+        public static void AddVariable(string name, float value)
+        {
+            variables.Add(name, value);
+        }
+
+        public static void RemoveVariable(string name)
+        {
+            variables.Remove(name);
+        }
+    }
+
+    public class ExpressionTreeCreator
+    {
+        private Stack<Expression> buffer;
+
+        public ExpressionTreeCreator()
+        {
+            buffer = new Stack<Expression>();
+        }
+
+        public Expression CreateTree(IEnumerable<Token> polishEntry)
+        {
+            foreach (var item in polishEntry)
+            {
+                switch (item.Type)
+                {
+                    case (TokenType.Number):
+                        buffer.Push(new NumberExpression(item.Value));
+                        break;
+                    case (TokenType.Variable):
+                        buffer.Push(new VariableExpression(item.Value));
+                        break;
+                    case (TokenType.Operator): 
+                    case (TokenType.Function):
+                        {
+                            var function = Context.GetFunction(item.Value);
+                            for (int i = Context.GetAgrumentsCount(item.Value) - 1; i >= 0; i--)
+                            {
+                                function.SetArgument(i, buffer.Pop());
+                            }
+                            buffer.Push(function);
+                            break;
+                        }
+                  
+                }
+            }
+            return buffer.Pop();
+        }
+
+
+        public abstract class Expression
+        {
+            public abstract float Calculate();
+        }
+
+        public abstract class OperationExpression : Expression
+        {
+            protected readonly Expression left;
+            protected readonly Expression right;
+
+            public OperationExpression(Expression left, Expression right)
+            {
+                this.left = left;
+                this.right = right;
+            }
+
+            public abstract OperationExpression CreateSameOperator(Expression left, Expression right);
+        }
+
+
+        public class PowOperator : FunctionExpression
+        {
+            public PowOperator() : base(2)
+            { }
+
+            public override float Calculate()
+            {
+                return (float)Math.Pow(GetArgument(0), GetArgument(1));
+            }
+
+            public override FunctionExpression Clone()
+            {
+                return new PowOperator();
+            }
+        }
+
+
+        public class MultiplyOperator : FunctionExpression
+        {
+            public MultiplyOperator() : base(2)
+            { }
+
+            public override float Calculate()
+            {
+                return GetArgument(0) * GetArgument(1);
+            }
+
+            public override FunctionExpression Clone()
+            {
+                return new MultiplyOperator();
+            }
+        }
+
+        public class DevideOperator : FunctionExpression
+        {
+            public DevideOperator() : base(2)
+            { }
+
+            public override float Calculate()
+            {
+                return GetArgument(0) / GetArgument(1);
+            }
+
+            public override FunctionExpression Clone()
+            {
+                return new DevideOperator();
+            }
+        }
+
+
+        public class SumOperator : FunctionExpression
+        {
+            public SumOperator() : base(2)
+            { }
+
+            public override float Calculate()
+            {
+                return GetArgument(0) + GetArgument(1);
+            }
+
+            public override FunctionExpression Clone()
+            {
+                return new SumOperator();
+            }
+        }
+
+        public class MinusOperator : FunctionExpression
+        {
+            public MinusOperator() : base(2)
+            { }
+
+            public override float Calculate()
+            {
+                return GetArgument(0) - GetArgument(1);
+            }
+
+            public override FunctionExpression Clone()
+            {
+                return new MinusOperator();
+            }
+        }
+
+
+
+
+        public sealed class VariableExpression : Expression
+        {
+            private string variableName;
+            public VariableExpression(string variableName)
+            {
+                this.variableName = variableName;
+            }
+
+            public override float Calculate()
+            {
+                return Context.GetVariableValue(variableName);
+            }
+        }
+        public sealed class NumberExpression : Expression
+        {
+            private float value;
+
+            public NumberExpression(float number)
+            {
+                value = number;
+            }
+
+            public NumberExpression(string number)
+            {
+                if (number.Contains('.'))
+                {
+                    number = number.Replace('.', ',');
+                }
+                value = float.Parse(number);
+            }
+
+            public override float Calculate()
+            {
+                return value;
+            }
+        }
+
+
+        public abstract class FunctionExpression : Expression
+        {
+            private Expression[] arguments;
+
+            protected FunctionExpression(Expression[] arguments)
+            {
+                this.arguments = arguments;
+            }
+
+            protected FunctionExpression(int argc)
+            {
+                this.arguments = new Expression[argc];
+            }
+
+            public void SetArgument(int index, Expression value)
+            {
+                arguments[index] = value;
+            }
+
+            public float GetArgument(int index)
+            {
+                return arguments[index].Calculate();
+            }
+
+            public abstract FunctionExpression Clone();
+        }
+
+        public sealed class SinExpression : FunctionExpression
+        {
+            public SinExpression(Expression expression) : base(1)
+            {
+                SetArgument(0, expression);
+            }
+
+            public SinExpression() : base(1)
+            { }
+
+            public override float Calculate()
+            {
+                return (float)Math.Sin(GetArgument(0));
+            }
+
+            public override FunctionExpression Clone()
+            {
+                return new SinExpression();
+            }
+        }
+
+        public sealed class MaxExpression : FunctionExpression
+        {
+            public MaxExpression(Expression left, Expression right) : base(2)
+            {
+                SetArgument(0, left);
+                SetArgument(1, right);
+            }
+
+            public MaxExpression() : base(2)
+            { }
+
+            public override float Calculate()
+            {
+                return Math.Max(GetArgument(0), GetArgument(1));
+            }
+
+            public override FunctionExpression Clone()
+            {
+                return new MaxExpression();
+            }
+        }
+
     }
 }
